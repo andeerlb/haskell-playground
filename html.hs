d48cb02e3e202a2830b4145600f062585a02448d
Anderson Babinski <andeerlb@gmail.com>
2025-10-22 19:49:00 -0300

    building html wrapper

---
+++ html.hs
@@ -0,0 +1,14 @@
+main = putStrLn myhtml
+
+myhtml = makehtml "My Page" "hello, world?"
+
+makehtml title content = 
+    html_ (head_ (title_ title) <> body_ content)
+
+html_ content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
