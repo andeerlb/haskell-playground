065f050d3a2d431b8ba2f20d61528ccd130391ba
Anderson Babinski <andeerlb@gmail.com>
2025-10-27 20:37:42 -0300

    using module with haskell

--- hello.hs
+++ hello.hs
@@ -1,4 +1,18 @@
-main = do
-    putStrLn "Hello, World!"
-    putStrLn "Welcome to Haskell programming."
-    putStrLn ("Please, look at my favorite odd numbers" ++ show (filter odd [1,3..19]))
+-- hello.hs
+
+import Html
+
+main :: IO ()
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
