9aae9caeb1a693581e9cc7471174e3cfa65086fc
Anderson Babinski <andeerlb@gmail.com>
2025-11-25 23:43:12 -0300

    range

--- ranges/Main.hs
+++ ranges/Main.hs
@@ -10,6 +10,11 @@ main = do
     let range7 = take 10 [1..]               -- Taking first 10 elements from the infinite list
     let range8 = [1,3..20]             -- List of odd numbers from 1 to 20
     let range9 = [20,18..1]            -- List of integers from 20 down to 1
+    let range10 = take 5 [0.5,1.0..]        -- Taking first 5 elements from an infinite list of floating-point numbers starting from 0.5
+    let range11 = take 7 ['A','C'..]          -- Taking first 7 uppercase letters starting from 'A' with a step of 2
+    let range12 = take 12 (cycle [1,2,3])  -- Taking first 12 elements from an infinite cyclic list of [1,2,3]
+    let range13 = take 10 (repeat 5)       -- Taking first 10 elements from an infinite list of 5s
+    let range14 = replicate 6 "Hello"  -- List with "Hello" repeated 6 times
 
     print range1
     print range2
